df452afc646249ed12579ae5ed2a2f372e5290ef
josiela <joseffa.steuernagel@haw-hamburg.de>
2022-04-15 16:30:54 +0200

    Removed stuff not existant screen imports

--- App.js
+++ App.js
@@ -12,12 +12,7 @@ import MensCycleScreen from "./screens/MensCycleScreen";
 import * as content from "./constants/texts";
 import ChangePWScreen from "./screens/ChangePWScreen";
 import IndexCal from "./screens/IndexCal";
-import PasswordCheck from "./screens/PasswordCheck";
 import IndexCircle from "./screens/IndexCircle";
-import MonasChoosePwScreen from "./screens/MonasChoosePWScreen";
-import MonasChangePwScreen from "./screens/MonasChangePWScreen";
-import MonasPasswordCheck from "./screens/MonasPasswordCheck";
-import MonasMensCycleScreen from "./screens/MonasMensCycleScreen";
 /**
  * The MASTER APP.
  * We can do it! *peptalk*
@@ -28,7 +23,7 @@ import MonasMensCycleScreen from "./screens/MonasMensCycleScreen";
 export default function App() {
   return (
     <View style={styles.container}>
-      <MonasMensCycleScreen />
+      <IndexCal />
     </View>
   );
 }

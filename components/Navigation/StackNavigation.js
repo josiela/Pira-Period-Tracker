02be3cd10fd80ca8818cf4689e861b6ad16dd7c4
Francie <Francie.Roessler@web.de>
2022-05-18 12:07:30 +0200

    last InputScreen with logic and updated StackNavigation. WIP

--- components/Navigation/StackNavigation.js
+++ components/Navigation/StackNavigation.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import SwipeNavigation from "../SwipeNavigation";
-import InfoTextScreen from "../../screens/InfoTextScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import "react-native-gesture-handler";
+
+// Our Screen Imports
+import InfoTextScreen from "../../screens/InfoTextScreen";
+import ChangePWScreen from "../../screens/ChangePWScreen";
+import MensCycleChangeScreen from "../../screens/MensCycleChangeScreen";
+import AboutUsScreen from "../../screens/AboutUsScreen";
+import InfoWOButtScreen from "../../screens/InfoWOButtScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +17,10 @@ const StackNavigation = (props) => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="home" component={SwipeNavigation} />
       <Stack.Screen name="Next page" component={InfoTextScreen} />
+      <Stack.Screen name="ChangePW" component={ChangePWScreen} />
+      <Stack.Screen name="MensundZyklus" component={MensCycleChangeScreen} />
+      <Stack.Screen name="AboutUs" component={AboutUsScreen} />
+      <Stack.Screen name="Info" component={InfoWOButtScreen} />
     </Stack.Navigator>
   );
 };
